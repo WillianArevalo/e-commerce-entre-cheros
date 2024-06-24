c43386d2cd0e66940f9db1b92614f0d7b3b9c87c
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-06-24 13:42:40 -0600

    Se actualizaron las rutas de la aplicación

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", [HomeController::class, "index"])->name("home");
+Route::controller(ProductController::class)->group(function () {
+    Route::get("/products/{id}/details", "details")->name("products.details");
 });
+
+Route::get("/cart", [CartController::class, "index"])->name("cart");
+Route::get("/checkout", [CheckoutController::class, "index"])->name("checkout");
+Route::get("/categorys", [CategoryController::class, "index"])->name("categorys");
