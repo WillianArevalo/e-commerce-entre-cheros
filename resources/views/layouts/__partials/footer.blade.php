c10fddd787c2d37d2b370d95ce7d6ac695847979
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-06-24 12:56:19 -0600

    Se creo el template base para la vista del usuario

---
+++ resources/views/layouts/__partials/footer.blade.php
@@ -0,0 +1,49 @@
+<footer class="w-full">
+    <div class="px-20 py-10 w-full bg-secondary flex justify-between">
+        <div class="flex gap-16">
+            <div>
+                <ul class="text-white font-primary flex flex-col gap-2 uppercase">
+                    <li><a class="text-xl font-primary font-bold text-white">Inicio</a></li>
+                    <li><a href="">Tienda</a></li>
+                    <li><a href="">Conocenos</a></li>
+                    <li><a href="">Preguntas frecuentes</a></li>
+                    <li><a href="">Contactanos</a></li>
+                </ul>
+            </div>
+            <div>
+                <h3 class="text-xl font-secondary font-bold text-white">Información</h3>
+                <ul class="text-gray-400">
+                    <li>Lorem ipsum</li>
+                    <li>Lorem ipsum</li>
+                    <li>Lorem ipsum</li>
+                </ul>
+            </div>
+            <div class="font-secondary">
+                <h3 class="text-xl  font-bold text-white">Legal</h3>
+                <ul class="text-white">
+                    <li><a href="" class="text-gray-400">Términos y condiciones</a></li>
+                    <li><a href="" class="text-gray-400">Politica y privacidad</a></li>
+                    <li><a href="" class="text-gray-400">Políticas de envío</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="flex flex-col gap-4">
+            <h2 class="text-xl font-bold font-primary text-white uppercase">Sigamos en contacto</h2>
+            <a href=""
+                class="rounded-2xl bg-primary px-5 py-2 w-max font-secondary text-secondary font-medium border-secondary border-2 hover:bg-tertiary hover:text-white ml-auto">
+                Iniciar sesión
+            </a>
+            <div class="flex gap-4 items-center justify-end">
+                <a href="" class="hover:text-tertiary text-primary hover:scale-110 transition-all">
+                    <x-icon icon="facebook" class="w-6 h-6 text-current" />
+                </a>
+                <a href="" class="hover:text-tertiary text-primary hover:scale-110 transition-all">
+                    <x-icon icon="x" class="w-6 h-6 text-current" />
+                </a>
+                <a href="" class="hover:text-tertiary text-primary hover:scale-110 transition-all">
+                    <x-icon icon="instagram" class="w-6 h-6 text-current" />
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/layouts/__partials/navbar.blade.php
@@ -0,0 +1,33 @@
+<header class="z-10 absolute top-0 w-full">
+    <nav class="bg-primary m-4 flex gap-2 justify-between py-2 px-4 rounded-full items-center w-9/12 mx-auto">
+        <div class="flex items-center gap-10">
+            <div class="flex items-center">
+                <img src="{{ asset('images/photo.jpg') }}" alt="Logo" class="object-cover rounded-full w-12 h-12">
+            </div>
+            <ul class="flex gap-6 text-secondary font-extrabold font-secondary">
+                <li><a href="{{ route('home') }}" class="uppercase">Inicio</a></li>
+                <li><a href="" class="uppercase">Tienda</a></li>
+                <li><a href="" class="uppercase">Conócenos</a></li>
+                <li><a href="" class="uppercase">Preguntas frecuentes</a></li>
+                <li><a href="" class="uppercase">Contactanos</a></li>
+            </ul>
+        </div>
+        <ul class="flex gap-4">
+            <li>
+                <a href="">
+                    <x-icon icon="search" class="w-6 h-6 text-secondary" />
+                </a>
+            </li>
+            <li>
+                <a href="{{ route('cart') }}">
+                    <x-icon icon="shopping-cart" class="w-6 h-6 text-secondary" />
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <x-icon icon="user-circle" class="w-6 h-6 text-secondary" />
+                </a>
+            </li>
+        </ul>
+    </nav>
+</header>
