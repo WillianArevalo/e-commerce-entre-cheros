4f69e0765936c416014bb5dd37b1586ddd09f798
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-06-25 14:43:01 -0600

    Se agrego la funcionalidad del cambio de tema

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,25 @@
 import "./bootstrap";
+import "./jquery";
 import "flowbite";
+import "./loginAdmin";
+
+$(document).ready(function () {
+    const $btnToggleTheme = $("#toggleTheme");
+    const $html = $("html");
+
+    if (localStorage.getItem("theme") === "dark") {
+        $html.addClass("dark");
+    } else {
+        $html.removeClass("dark");
+    }
+
+    $btnToggleTheme.on("click", function () {
+        if ($html.hasClass("dark")) {
+            $html.removeClass("dark");
+            localStorage.setItem("theme", "light");
+        } else {
+            $html.addClass("dark");
+            localStorage.setItem("theme", "dark");
+        }
+    });
+});
