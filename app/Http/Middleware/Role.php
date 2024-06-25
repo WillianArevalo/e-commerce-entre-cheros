ed74ef77fde74984c4e5d7fcf82536e992b0231a
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-06-25 14:39:02 -0600

    Se agrego el middleware para validar el rol del usuario

---
+++ app/Http/Middleware/Role.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class Role
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $role): Response
+    {
+
+        if (!Auth::check()) {
+
+            if ($request->is("admin")) {
+                return redirect("/admin/login");
+            }
+
+            return redirect("/login");
+        }
+
+        $user = Auth::user();
+
+        if ($user->role != $role) {
+            switch ($user->role) {
+                case "admin":
+                    redirect("/admin");
+                    break;
+                case "user":
+                    redirect("/");
+                    break;
+            }
+        }
+        return $next($request);
+    }
+}
