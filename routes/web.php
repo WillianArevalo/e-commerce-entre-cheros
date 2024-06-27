df272140775ff18781d4c68d08ad623e8efa5aa5
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-06-27 16:38:57 -0600

    Se agregaron nuevas rutas al proyecto

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,17 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CartController;
-use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CategorieController;
 use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SubCategorieController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+
+/** Routes Store */
 Route::get("/", [HomeController::class, "index"])->name("home");
 Route::controller(ProductController::class)->group(function () {
     Route::get("/products/{id}/details", "details")->name("products.details");
@@ -18,14 +21,18 @@ Route::controller(ProductController::class)->group(function () {
 
 Route::get("/cart", [CartController::class, "index"])->name("cart");
 Route::get("/checkout", [CheckoutController::class, "index"])->name("checkout");
-Route::get("/categorys", [CategoryController::class, "index"])->name("categorys");
-
+Route::get("/categories", [CategorieController::class, "showCategoriesStore"])->name("categories");
 Route::get("/login", [AuthController::class, "showLoginForm"])->name("login");
 
+
+/** Routes Admin */
 Route::get("/admin/login", [AdminController::class, "login"])->name("admin.login");
 Route::post("/admin/validate", [AuthController::class, "validateAdmin"])->name("admin.validate");
+
 Route::middleware("role:admin")->prefix("admin")->name("admin.")->group(function () {
     Route::get("/", [AdminController::class, "index"])->name("index");
+    Route::resource("/categories", CategorieController::class);
+    Route::resource("/subcategories", SubCategorieController::class);
 });
 
 Route::post("/logout", [AuthController::class, "logout"])->name("logout");
