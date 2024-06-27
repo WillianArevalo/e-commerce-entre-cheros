72e441f9727bc381eb2b3afcf024775ff0714509
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-06-27 16:41:06 -0600

    Se agrego la vista principal del dashboard del administrador

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,11 +3,8 @@
 @section('title', 'Admin | Dashboard')
 
 @section('content')
-    <main class="bg-gray-50 dark:bg-gray-900 h-screen">
-        <div class="p-4 sm:ml-72">
-            <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-                <h1>Bienvenido al dashboard</h1>
-            </div>
-        </div>
-    </main>
+    <div
+        class="dark:text-white text-gray-400 p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-4">
+        <h1 class="font-bold text-primary text-3xl">Bienvenido al dashboard</h1>
+    </div>
 @endsection
