69d501ddb40899f1f8804085e0e59745dcea69c5
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-06-27 16:37:33 -0600

    Se creo un modal para eliminar categorias

---
+++ app/View/Components/DeleteModal.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class DeleteModal extends Component
+{
+
+    public $modalId;
+    public $title;
+    public $message;
+    public $action;
+    /**
+     * Create a new component instance.
+     */
+    public function __construct($modalId, $title, $message, $action)
+    {
+        $this->modalId = $modalId;
+        $this->title = $title;
+        $this->message = $message;
+        $this->action = $action;
+    }
+
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.delete-modal');
+    }
+}
