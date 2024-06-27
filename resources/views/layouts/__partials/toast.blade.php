34bda85983d36aba49f59bd071e8c60d5ec783a4
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-06-27 16:38:38 -0600

    Se actualizaron los estilos del partial del toast

--- resources/views/layouts/__partials/toast.blade.php
+++ resources/views/layouts/__partials/toast.blade.php
@@ -1,6 +1,6 @@
 @if ($message = Session::get('success'))
-    <div id="toast-danger"
-        class="fixed flex items-center gap-4 justify-between w-full max-w-xs p-4  text-gray-500 bg-white rounded-lg shadow top-5 right-5 dark:text-gray-400 dark:bg-gray-800 font-secondary"
+    <div id="toast-success"
+        class="fixed flex items-center gap-4 justify-between w-full max-w-xs p-4  text-gray-500 bg-white rounded-lg shadow top-5 right-5 dark:text-gray-400 dark:bg-gray-800 font-secondary z-50 animate-fade-left animate-once animate-duration-300"
         role="alert">
         <div
             class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-lg bg-green-500 dark:bg-green-800">
@@ -9,7 +9,7 @@
         <div class="ms-auto text-sm font-normal">{{ $message }}</div>
         <button type="button"
             class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300  hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
-            data-dismiss-target="#toast-danger" aria-label="Close">
+            data-dismiss-target="#toast-success" aria-label="Close">
             <span class="sr-only">Close</span>
             <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                 viewBox="0 0 14 14">
@@ -20,7 +20,7 @@
     </div>
 @elseif($message = Session::get('error'))
     <div id="toast-danger"
-        class="fixed flex items-center gap-4 justify-between w-full max-w-xs p-4  text-gray-500 bg-white rounded-lg shadow top-5 right-5 dark:text-gray-400 dark:bg-gray-800 font-secondary"
+        class="fixed flex items-center gap-4 justify-between w-full max-w-xs p-4  text-gray-500 bg-white rounded-lg shadow top-5 right-5 dark:text-gray-400 dark:bg-gray-800 font-secondary z-50"
         role="alert">
         <div
             class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8  rounded-lg bg-red-500 dark:bg-red-800">
