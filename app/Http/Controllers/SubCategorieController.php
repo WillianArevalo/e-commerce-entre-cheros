fe2f40658dbf28bfc148b949440f6ea7bf58f354
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-07-01 20:50:21 -0600

    Se agrego al función de buscar que retorna las subcategorías de una categoría principal

--- app/Http/Controllers/SubCategorieController.php
+++ app/Http/Controllers/SubCategorieController.php
@@ -34,4 +34,20 @@ class SubCategorieController extends Controller
         $subcategorie->delete();
         return redirect()->route("admin.categories.index")->with("success", "Subcategoría eliminada correctamente");
     }
+
+    public function search(Request $request)
+    {
+        $query = SubCategorie::where("categorie_id", $request->input("categorie_id"));
+        $subcategories = $query->get();
+        if ($request->ajax()) {
+            $subcategorieSelected["name"] = "No tiene subcategorías";
+            if ($subcategories->first()) {
+                $subcategorieSelected = $subcategories->first();
+            }
+            $html = view("layouts.__partials.ajax.option-subcategorie", compact("subcategories"))->render();
+            return response()->json(["message" => "Encontrado", "html" => $html, "subcategoria" => $subcategorieSelected], 201);
+        } else {
+            return response()->json(["message" => "No encontrado"], 400);
+        }
+    }
 }
