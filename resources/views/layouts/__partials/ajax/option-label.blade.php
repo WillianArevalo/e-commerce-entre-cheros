da6daf286673df90c872ce818fe8e790f3f4ddc5
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-07-03 21:18:35 -0600

    Se actualizo la vista parcial de las opciones de etiquetas de la resputa del ajax

--- resources/views/layouts/__partials/ajax/option-label.blade.php
+++ resources/views/layouts/__partials/ajax/option-label.blade.php
@@ -1,7 +1,7 @@
    @if ($labels->count() > 0)
        @foreach ($labels as $label)
            <li class="itemOption text-sm text-gray-900 dark:text-white px-4 py-2.5 hover:bg-gray-100 dark:hover:bg-gray-700"
-               data-value="{{ $label->name }}" data-input="label_id">
+               data-value="{{ $label->name }}" data-input="#label_id">
                {{ $label->name }}
            </li>
        @endforeach
