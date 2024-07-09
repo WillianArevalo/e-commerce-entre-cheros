dd15422b560ebd3ad40f3c842165b00f115c85d7
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-07-08 18:56:00 -0600

    Se agrego la relación de usuario con cliente

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,11 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+    public function customer()
+    {
+        return $this->hasOne(Customer::class);
+    }
+
     /**
      * The attributes that are mass assignable.
      *
