6e825e76a6c4e86b7da2a7aca1510d38d17af974
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-07-20 19:37:01 -0600

    Se actualizo el controlador del home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Categorie;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $products = Product::whereDoesntHave("flash_offers")->get();
+        $categories = Categorie::all();
+        $flashOffers = Product::whereHas('flash_offers', function ($query) {
+            $query->where('is_showing', true)
+                ->where('is_active', true);
+        })->with(['flash_offers' => function ($query) {
+            $query->where('is_showing', true)
+                ->where('is_active', true);
+        }])->get();
+        return view('home', ["products" => $products, "flashOffers" => $flashOffers, "categories" => $categories]);
     }
 }
