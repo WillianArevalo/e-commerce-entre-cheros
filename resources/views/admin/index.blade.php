9014cbaaceb679f9507d39e9c0e3ffe95efc583f
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-07-20 19:46:29 -0600

    Se actualizo la vista del index del admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,6 +5,8 @@
 @section('content')
     <div
         class="dark:text-white text-gray-400 p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-4">
-        <h1 class="font-bold text-primary text-3xl">Bienvenido al dashboard</h1>
+        <h1 class="font-bold text-primary text-3xl">
+            {{ __('messages.welcome') }}
+        </h1>
     </div>
 @endsection
