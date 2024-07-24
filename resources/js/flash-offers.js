04ea0ab7582033e9a972910803a033663452ec12
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-07-23 20:52:24 -0600

    Se actualizo el script de las ofertas relampago

--- resources/js/flash-offers.js
+++ resources/js/flash-offers.js
@@ -1,4 +1,52 @@
+import { showOverlay, hideOverlay, closeDrawer } from "./drawer";
+
 $(document).ready(function () {
+    $("#new-flash-offer").on("click", function () {
+        $("#drawer-new-flash-offer").removeClass("translate-x-full");
+        showOverlay();
+    });
+
+    function closeDrawers() {
+        closeDrawer("#drawer-new-flash-offer");
+        closeDrawer("#drawer-edit-flash-offer");
+    }
+
+    $(".close-drawer-offer").on("click", function () {
+        closeDrawers();
+    });
+
+    $("#overlay").on("click", function () {
+        closeDrawers();
+    });
+
+    $(document).on("click", ".editFlashOffer", function () {
+        const href = $(this).data("href");
+        const action = $(this).data("action");
+        $.ajax({
+            type: "GET",
+            url: href,
+            success: function (response) {
+                showOverlay();
+                $("#drawer-edit-flash-offer").removeClass("translate-x-full");
+                $("#formEditFlashOffer").attr("action", action);
+                $("#product_id").val(response.product.id);
+                $("#product_name").val(response.product.name);
+                $("#offer_price_edit").val(response.product.offer_price);
+                $("#start_date_edit").val(response.offer.start_date);
+                $("#end_date_edit").val(response.offer.end_date);
+                $("#is_showing_edit").val(response.offer.is_showing);
+
+                if (response.offer.is_showing === 1) {
+                    $("#is_showing_edit").attr("checked", true);
+                }
+
+                if (response.offer.is_active === 1) {
+                    $("#is_active_edit").attr("checked", true);
+                }
+            },
+        });
+    });
+
     $(".toggleShow").on("change", function () {
         const form = $($(this).data("form"));
         const isChecked = $(this).is(":checked");
