93118f5379e744fbb58451825e089ce253188697
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-07-23 20:50:14 -0600

    Se actualizaron los links del navbar

--- resources/views/layouts/__partials/navbar.blade.php
+++ resources/views/layouts/__partials/navbar.blade.php
@@ -20,7 +20,7 @@
         </div>
         <ul class="flex gap-6 items-center">
             <li>
-                <a href="" class="text-secondary hover:text-rose-500">
+                <a href="{{ route('favorites') }}" class="text-secondary hover:text-rose-500">
                     <x-icon icon="favourite" class="w-6 h-6 text-current hover:fill-rose-500" />
                 </a>
             </li>
@@ -33,20 +33,20 @@
                     </div>
                 </a>
             </li>
-            <li class="relative group">
+            <li class="relative group flex items-center">
                 @if ($user = auth()->user())
                     <button type="button">
                         <img src="{{ Storage::url($user->profile_photo_path) }}" alt=""
                             class="w-10 h-10 object-cover rounded-full">
                     </button>
                     <div
-                        class="absolute bg-white rounded-lg w-40 font-secondary text-sm overflow-hidden hidden group-hover:block shadow-md">
+                        class="absolute bg-white rounded-lg w-40 font-secondary text-sm overflow-hidden hidden group-hover:block shadow-md top-10">
                         <ul class="flex flex-col">
                             <li class="px-4 py-2 hover:bg-zinc-100 w-full">
                                 <a href="" class="block w-full">Perfil</a>
                             </li>
                             <li class="px-4 py-2 hover:bg-zinc-100 w-full">
-                                <a href="" class="block w-full">Favoritos</a>
+                                <a href="{{ route('favorites') }}" class="block w-full">Favoritos</a>
                             </li>
                             <li class="px-4 py-2 hover:bg-zinc-100 w-full">
                                 <a href="" class="block w-full">Pedidos</a>
