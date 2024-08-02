303f9d8b1936e974e8c4edab1a1d0110bb6f072e
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-01 23:49:56 -0600

    Se actualizon las vistas parciales del ajax

--- resources/views/layouts/__partials/ajax/option-subcategorie.blade.php
+++ resources/views/layouts/__partials/ajax/option-subcategorie.blade.php
@@ -1,6 +1,6 @@
  @if ($subcategories->count() > 0)
      @foreach ($subcategories as $subcategorie)
-         <li class="itemOption text-sm text-gray-900 dark:text-white px-4 py-2.5 hover:bg-gray-100 dark:hover:bg-zinc-900 rounded"
+         <li class="itemOption rounded-lg px-4 py-2.5 text-sm text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-zinc-900"
              data-value="{{ $subcategorie->id }}" data-input="#subcategorie_id">
              {{ $subcategorie->name }}
          </li>
