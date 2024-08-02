9e259315b79efa282e0a23174c36bff19ca3ef16
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-01 23:51:36 -0600

    Se actualizo la vista del breadcrumb y el toast

--- resources/views/layouts/__partials/breadcrumb.blade.php
+++ resources/views/layouts/__partials/breadcrumb.blade.php
@@ -2,24 +2,24 @@
 @php $breadcrumbs = Breadcrumbs::generate(); @endphp
 @if (!empty($breadcrumbs))
     <div class="flex justify-end">
-        <nav class="flex text-gray-700 w-max" aria-label="Breadcrumb">
-            <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse uppercase">
+        <nav class="flex w-max px-6 py-2 text-gray-700" aria-label="Breadcrumb">
+            <ol class="inline-flex items-center space-x-1 uppercase rtl:space-x-reverse md:space-x-2">
                 @foreach ($breadcrumbs as $key => $breadcrumb)
                     @if ($breadcrumb->url && !$loop->last)
                         <li>
                             <div class="flex items-center gap-4">
                                 <a href="{{ $breadcrumb->url }}"
-                                    class="text-sm font-medium text-gray-700 hover:text-blue-600  dark:text-gray-400 dark:hover:text-white flex items-center gap-1">
+                                    class="flex items-center gap-1 rounded-lg px-3 py-1 text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:bg-zinc-950 dark:hover:text-white">
                                     {{ $breadcrumb->title }}
                                 </a>
-                                <x-icon icon="arrow-right" class="rtl:rotate-180 block w-5 h-5 mx-1 text-gray-400 " />
+                                <x-icon icon="arrow-right" class="mx-1 block h-5 w-5 text-gray-400 rtl:rotate-180" />
                             </div>
                         </li>
                     @else
                         <li aria-current="page">
                             <div class="flex items-center">
                                 <span
-                                    class="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">{{ $breadcrumb->title }}</span>
+                                    class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400 md:ms-2">{{ $breadcrumb->title }}</span>
                             </div>
                         </li>
                     @endif
--- resources/views/layouts/__partials/toast.blade.php
+++ resources/views/layouts/__partials/toast.blade.php
@@ -1,17 +1,17 @@
 @if ($message = Session::get('success'))
     <div id="toast-success"
-        class="fixed flex items-center gap-4 justify-between w-full max-w-xs p-4  text-gray-500 bg-white rounded-lg shadow top-5 right-5 dark:text-gray-400 dark:bg-gray-800 font-secondary z-50 animate-fade-left animate-once animate-duration-300"
+        class="fixed right-5 top-5 z-50 flex w-full max-w-xs animate-fade-left items-center justify-between gap-4 rounded-lg bg-white p-4 font-secondary text-gray-500 shadow animate-duration-300 animate-once dark:bg-zinc-950 dark:text-gray-400"
         role="alert">
         <div
-            class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 rounded-lg bg-green-500 dark:bg-green-800">
-            <x-icon icon="checkmark-circle" class="w-5 h-5 text-white" />
+            class="inline-flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-lg bg-green-500 dark:bg-green-800">
+            <x-icon icon="checkmark-circle" class="h-5 w-5 text-white" />
         </div>
         <div class="ms-auto text-sm font-normal">{{ $message }}</div>
         <button type="button"
-            class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300  hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+            class="-mx-1.5 -my-1.5 ms-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-white text-gray-400 hover:bg-gray-100 hover:text-gray-900 focus:ring-2 focus:ring-gray-300 dark:bg-zinc-900 dark:text-gray-500 dark:hover:bg-zinc-800 dark:hover:text-white"
             data-dismiss-target="#toast-success" aria-label="Close">
             <span class="sr-only">Close</span>
-            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+            <svg class="h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                 viewBox="0 0 14 14">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
@@ -20,18 +20,18 @@
     </div>
 @elseif($message = Session::get('error'))
     <div id="toast-danger"
-        class="fixed flex items-center gap-4 justify-between w-full max-w-xs p-4  text-gray-500 bg-white rounded-lg shadow top-5 right-5 dark:text-gray-400 dark:bg-gray-800 font-secondary z-50"
+        class="fixed right-5 top-5 z-50 flex w-full max-w-xs items-center justify-between gap-4 rounded-lg bg-white p-4 font-secondary text-gray-500 shadow dark:bg-zinc-950 dark:text-gray-400"
         role="alert">
         <div
-            class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8  rounded-lg bg-red-500 dark:bg-red-800">
-            <x-icon icon="alert-circle" class="w-5 h-5 text-white" />
+            class="inline-flex h-8 w-8 flex-shrink-0 items-center justify-center rounded-lg bg-red-500 dark:bg-red-800">
+            <x-icon icon="alert-circle" class="h-5 w-5 text-white" />
         </div>
         <div class="ms-auto text-sm font-normal">{{ $message }}</div>
         <button type="button"
-            class="ms-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300  hover:bg-gray-100 inline-flex items-center justify-center h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+            class="-mx-1.5 -my-1.5 ms-auto inline-flex h-8 w-8 items-center justify-center rounded-lg bg-white text-gray-400 hover:bg-gray-100 hover:text-gray-900 focus:ring-2 focus:ring-gray-300 dark:bg-zinc-900 dark:text-gray-500 dark:hover:bg-zinc-800 dark:hover:text-white"
             data-dismiss-target="#toast-danger" aria-label="Close">
             <span class="sr-only">Close</span>
-            <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+            <svg class="h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                 viewBox="0 0 14 14">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
