a6b32f7d780c4e8d5202b8645dc0e99175594c2d
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-02 19:08:42 -0600

    Se actualizo el script de swiper.js

--- resources/js/swiper.js
+++ resources/js/swiper.js
@@ -1,24 +1,45 @@
 export function initSwiper() {
-    new Swiper(".mySwiper", {
-        slidesPerView: 3,
-        spaceBetween: 20,
-        navigation: {
-            nextEl: ".button-next",
-            prevEl: ".button-prev",
-        },
-        breakpoints: {
-            320: {
-                slidesPerView: 1,
-                spaceBetween: 20,
+    if (document.querySelector(".mySwiper")) {
+        new Swiper(".mySwiper", {
+            slidesPerView: 3,
+            spaceBetween: 20,
+            navigation: {
+                nextEl: ".button-next",
+                prevEl: ".button-prev",
             },
-            640: {
-                slidesPerView: 2,
-                spaceBetween: 20,
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                    spaceBetween: 20,
+                },
+                640: {
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+                },
+                1024: {
+                    slidesPerView: 3,
+                    spaceBetween: 20,
+                },
             },
-            1024: {
-                slidesPerView: 3,
-                spaceBetween: 20,
+        });
+    }
+
+    if (document.querySelector(".swiper-home")) {
+        new Swiper(".swiper-home", {
+            slidesPerView: 1,
+            loop: true,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
             },
-        },
-    });
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+            navigation: {
+                nextEl: ".button-next-home",
+                prevEl: ".button-prev-home",
+            },
+        });
+    }
 }
