2938db9df3a1153ff3a633b6f5179b730879b7b7
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-15 16:01:02 -0600

    Se agrego la vista del loader

---
+++ resources/views/layouts/__partials/store/loader.blade.php
@@ -0,0 +1,7 @@
+     <div id="loader" class="loader hidden">
+         <div class="spinner">
+             <span class="flex items-center justify-center">
+                 <x-icon-store icon="bird" class="h-10 w-10 text-secondary" />
+             </span>
+         </div>
+     </div>
