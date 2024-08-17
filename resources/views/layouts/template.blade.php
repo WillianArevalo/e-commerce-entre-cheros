7ca753c792f870109a3c7572f1dda19fc9110f64
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-17 13:42:00 -0600

    Se cambio el favicon de la aplicación

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link rel="shortcut icon" href="{{ asset('images/logo de entre cheros.png') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('images/imagen6.png') }}" type="image/x-icon">
     @vite('resources/css/app.css')
     @vite('resources/css/home.css')
     @vite('resources/js/app.js')
