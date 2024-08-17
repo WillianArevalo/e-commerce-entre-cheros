3afa86bfb1cb68236324541526b0238ada4b7b9c
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-17 13:40:49 -0600

    Se ha agregado una nueva función al helper de favoritos

--- app/Helpers/Favorites.php
+++ app/Helpers/Favorites.php
@@ -2,6 +2,9 @@
 
 namespace App\Helpers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
 class Favorites
 {
     public static function get($user, $products)
@@ -16,5 +19,17 @@ class Favorites
                 $product->is_favorite = false;
             });
         }
+    }
+
+    public static function count()
+    {
+        $auth = Auth::user();
+        if ($auth) {
+            $user = User::find($auth->id);
+            if ($user) {
+                return $user->favorites()->count();
+            }
+        }
+        return 0;
     }
 }
