9a6c50474ad1fb81181edacb499c8b028a9dea5b
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-27 14:44:05 -0600

    Se actualizo la migración de la tabla de clientes

--- database/migrations/2024_07_07_232555_create_customers_table.php
+++ database/migrations/2024_07_07_232555_create_customers_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string("phone");
-            $table->date("birthdate");
+            $table->string("phone")->nullable();
+            $table->date("birthdate")->nullable();
             $table->enum("gender", ["male", "female", "other"])->nullable();
             $table->string("status")->default("active");
             $table->foreignId("user_id")->constrained();
