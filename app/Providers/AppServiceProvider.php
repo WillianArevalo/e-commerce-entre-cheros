541ea600061b808363c454289c3cb8ac984c8696
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-27 14:46:53 -0600

    Se agrego el código para mostrar las monedas en el footer del usuario

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Currency;
+use App\Models\PaymentMethod;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer("layouts.__partials.store.footer", function ($view) {
+            $currencies = Currency::where("active", true)->get();
+            $currencyDefault = Currency::where("is_default", true)->first();
+            $paymentMethods = PaymentMethod::where("active", true)->get();
+            $view->with(
+                [
+                    "currencies" => $currencies,
+                    "currencyDefault" => $currencyDefault,
+                    "paymentMethods" => $paymentMethods
+                ]
+            );
+        });
     }
 }
