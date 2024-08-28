19ede4eab3a4462e9af594c1fb3a08e34d68dbdb
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-27 22:25:20 -0600

    Se agrego el featured de policies

--- resources/views/admin/policies/index.blade.php
+++ resources/views/admin/policies/index.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.admin-template')
-
-@section('title', 'Ajustes generales')
-
+@section('title', 'Políticas')
 @section('content')
     <div class="mt-4 dark:border-zinc-900 dark:bg-black">
         <div class="flex flex-col items-start border-y px-4 py-4 shadow-sm dark:border-zinc-900 dark:bg-black">
@@ -34,82 +32,193 @@
                 </li>
             </ul>
         </div>
-        <div class="m-4 rounded-lg p-4 dark:bg-black">
-            <p class="mb-4 text-sm text-zinc-700 dark:text-zinc-400">Carga los archivos con las políticas correspondientes
-            </p>
-            <div class="grid grid-cols-3 gap-4">
-                <div
-                    class="flex flex-col items-center justify-center rounded-lg border border-zinc-300 dark:border-zinc-900">
-                    <h4 class="w-full border-b border-zinc-700 p-4 text-center text-sm text-zinc-700 dark:text-zinc-300">
-                        Políticas de privacidad
-                    </h4>
-                    <div class="p-2">
-                        <img src="{{ asset('images/photo.jpg') }}" alt="Imagen" class="h-full w-full object-cover">
+        <div class="bg-zinc-50 dark:bg-black">
+            <div class="mx-auto w-full">
+                <div class="relative bg-white shadow-md dark:bg-black">
+                    <div
+                        class="flex flex-col items-center justify-between space-y-3 p-4 md:flex-row md:space-x-4 md:space-y-0">
+                        <div class="w-full md:w-1/2">
+                            <form class="flex items-center" action="{{ route('admin.categories.search') }}"
+                                id="formSearchProduct">
+                                @csrf
+                                <x-input type="text" id="inputSearch" name="inputSearch" data-form="#formSearchProduct"
+                                    data-table="#tableProduct" placeholder="Buscar" icon="search" />
+                            </form>
+                        </div>
+                        <div
+                            class="flex w-full flex-shrink-0 flex-col items-stretch justify-end space-y-2 md:w-auto md:flex-row md:items-center md:space-x-3 md:space-y-0">
+                            <x-button data-drawer="#drawer-new-policie" class="open-drawer" type="button"
+                                text="Nueva política" icon="add-circle" typeButton="primary" />
+                            <div class="flex w-full items-center space-x-3 md:w-auto">
+                                <x-button type="button" id="filterDropdownButton" data-dropdown-toggle="filterDropdown"
+                                    typeButton="secondary" icon="filter" text="Filtros" />
+                                <div id="filterDropdown"
+                                    class="z-10 hidden w-48 rounded-lg bg-white p-3 shadow dark:bg-zinc-950">
+                                    <form action="{{ route('admin.categories.search') }}" method="POST"
+                                        id="formSearchCategorieCheck">
+                                        @csrf
+                                        <h6 class="mb-3 text-sm font-medium text-zinc-900 dark:text-white">
+                                            Filtros
+                                        </h6>
+                                        <ul class="space-y-2 text-sm" aria-labelledby="filterDropdownButton">
+                                            <li class="flex items-center">
+                                                <input id="offers" name="filter[]" type="checkbox" value="offers"
+                                                    class="h-4 w-4 rounded border-zinc-300 bg-zinc-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-zinc-500 dark:bg-white dark:ring-offset-zinc-700 dark:focus:ring-blue-600">
+                                                <label for="offers"
+                                                    class="ml-2 text-sm font-medium text-zinc-900 dark:text-zinc-100">
+                                                    Con ofertas
+                                                </label>
+                                            </li>
+                                            <li class="flex items-center">
+                                                <input id="flash_offers" name="filter[]" type="checkbox"
+                                                    value="flash_offers"
+                                                    class="h-4 w-4 rounded border-zinc-300 bg-zinc-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-zinc-500 dark:bg-white dark:ring-offset-zinc-700 dark:focus:ring-blue-600">
+                                                <label for="flash_offers"
+                                                    class="ml-2 text-sm font-medium text-zinc-900 dark:text-zinc-100">
+                                                    Con ofertas flash
+                                                </label>
+                                            </li>
+                                        </ul>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="flex w-full items-center justify-end gap-2 border-t border-zinc-700 p-2">
-                        <x-button type="a" text="Agregar PDF" typeButton="primary" icon="pdf" />
-                        <x-button type="a" typeButton="secondary" icon="view" />
-                        <x-button type="a" typeButton="danger" icon="delete" />
-                        <x-button type="button" typeButton="success" icon="import" />
-                    </div>
-                </div>
-                <div class="flex flex-col items-center justify-center rounded-lg border border-zinc-700">
-                    <h4 class="w-full border-b border-zinc-700 p-4 text-center text-sm text-zinc-700 dark:text-zinc-300">
-                        Políticas de privacidad
-                    </h4>
-                    <div class="p-2">
-                        <img src="{{ asset('images/photo.jpg') }}" alt="Imagen" class="h-full w-full object-cover">
-                    </div>
-                    <div class="flex w-full items-center justify-end gap-2 border-t border-zinc-700 p-2">
-                        <x-button type="a" text="Agregar PDF" typeButton="primary" icon="pdf" />
-                        <x-button type="a" typeButton="secondary" icon="view" />
-                        <x-button type="a" typeButton="danger" icon="delete" />
-                        <x-button type="button" typeButton="success" icon="import" />
-                    </div>
-                </div>
-                <div class="flex flex-col items-center justify-center rounded-lg border border-zinc-700">
-                    <h4 class="w-full border-b border-zinc-700 p-4 text-center text-sm text-zinc-700 dark:text-zinc-300">
-                        Políticas de privacidad
-                    </h4>
-                    <div class="p-2">
-                        <img src="{{ asset('images/photo.jpg') }}" alt="Imagen" class="h-full w-full object-cover">
-                    </div>
-                    <div class="flex w-full items-center justify-end gap-2 border-t border-zinc-700 p-2">
-                        <x-button type="a" text="Agregar PDF" typeButton="primary" icon="pdf" />
-                        <x-button type="a" typeButton="secondary" icon="view" />
-                        <x-button type="a" typeButton="danger" icon="delete" />
-                        <x-button type="button" typeButton="success" icon="import" />
-                    </div>
-                </div>
-                <div class="flex flex-col items-center justify-center rounded-lg border border-zinc-700">
-                    <h4 class="w-full border-b border-zinc-700 p-4 text-center text-sm text-zinc-700 dark:text-zinc-300">
-                        Políticas de privacidad
-                    </h4>
-                    <div class="p-2">
-                        <img src="{{ asset('images/photo.jpg') }}" alt="Imagen" class="h-full w-full object-cover">
-                    </div>
-                    <div class="flex w-full items-center justify-end gap-2 border-t border-zinc-700 p-2">
-                        <x-button type="a" text="Agregar PDF" typeButton="primary" icon="pdf" />
-                        <x-button type="a" typeButton="secondary" icon="view" />
-                        <x-button type="a" typeButton="danger" icon="delete" />
-                        <x-button type="button" typeButton="success" icon="import" />
-                    </div>
-                </div>
-                <div class="flex flex-col items-center justify-center rounded-lg border border-zinc-700">
-                    <h4 class="w-full border-b border-zinc-700 p-4 text-center text-sm text-zinc-700 dark:text-zinc-300">
-                        Políticas de privacidad
-                    </h4>
-                    <div class="p-2">
-                        <img src="{{ asset('images/photo.jpg') }}" alt="Imagen" class="h-full w-full object-cover">
-                    </div>
-                    <div class="flex w-full items-center justify-end gap-2 border-t border-zinc-700 p-2">
-                        <x-button type="a" text="Agregar PDF" typeButton="primary" icon="pdf" />
-                        <x-button type="a" typeButton="secondary" icon="view" />
-                        <x-button type="a" typeButton="danger" icon="delete" />
-                        <x-button type="button" typeButton="success" icon="import" />
+                    <div class="mx-4 mb-4 overflow-hidden rounded-lg border border-zinc-200 dark:border-zinc-900">
+                        <table class="w-full text-left text-sm text-zinc-500 dark:text-zinc-400">
+                            <thead
+                                class="border-b border-zinc-200 bg-zinc-50 text-xs uppercase text-zinc-700 dark:border-zinc-900 dark:bg-black dark:text-zinc-300">
+                                <tr>
+                                    <th scope="col" class="border-e border-zinc-200 px-4 py-3 dark:border-zinc-900">
+                                        <input id="default-checkbox" type="checkbox" value=""
+                                            class="h-4 w-4 rounded border-2 border-zinc-300 bg-zinc-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-zinc-900 dark:bg-zinc-950 dark:ring-offset-zinc-800 dark:focus:ring-blue-600">
+                                    </th>
+                                    <th scope="col" class="border-e border-zinc-200 px-4 py-3 dark:border-zinc-900">
+                                        Nombre
+                                    </th>
+                                    <th scope="col" class="border-e border-zinc-200 px-4 py-3 dark:border-zinc-900">
+                                        Path
+                                    </th>
+                                    <th scope="col" class="px-4 py-3">
+                                        Acciones
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @if ($policies->count() > 0)
+                                    @foreach ($policies as $policy)
+                                        <tr class="hover:bg-zinc-100 dark:hover:bg-zinc-950">
+                                            <td class="px-4 py-3">
+                                                <input id="default-checkbox" type="checkbox" value="{{ $policy->id }}"
+                                                    class="h-4 w-4 rounded border-2 border-zinc-300 bg-zinc-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-zinc-900 dark:bg-zinc-950 dark:ring-offset-zinc-800 dark:focus:ring-blue-600">
+                                            </td>
+                                            <td class="px-4 py-3">
+                                                <span>{{ $policy->name }}</span>
+                                            </td>
+                                            <td class="px-4 py-3">
+                                                <span>{{ $policy->file_path }}</span>
+                                            </td>
+                                            <td class="px-4 py-3">
+                                                <div class="flex gap-2">
+                                                    <form action="{{ route('admin.policies.destroy', $policy->id) }}"
+                                                        id="formDeletePolicie-{{ $policy->id }}" method="POST">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <x-button type="button"
+                                                            data-form="formDeletePolicie-{{ $policy->id }}"
+                                                            onlyIcon="true" icon="delete" typeButton="danger"
+                                                            class="buttonDelete" />
+                                                    </form>
+                                                    <x-button type="a" href="{{ Storage::url($policy->file_path) }}"
+                                                        target="_blank" onlyIcon="true" icon="view"
+                                                        typeButton="secondary" />
+                                                    <form action="{{ Route('admin.policies.download', $policy->id) }}"
+                                                        method="POST">
+                                                        @csrf
+                                                        <x-button type="submit" onlyIcon="true" icon="import"
+                                                            typeButton="success" />
+                                                    </form>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                @else
+                                    <tr>
+                                        <td class="px-4 py-3 text-center" colspan="4">
+                                            No hay políticas registradas
+                                        </td>
+                                    </tr>
+                                @endif
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
+
+        <x-delete-modal modalId="deleteModal" title="¿Estás seguro de eliminar la política?"
+            message="No podrás recuperar este registro" action="" />
+
+        <div id="drawer-new-policie"
+            class="drawer fixed right-0 top-0 z-40 h-screen w-[500px] translate-x-full overflow-y-auto bg-white p-4 transition-transform dark:bg-black"
+            tabindex="-1" aria-labelledby="drawer-new-policie">
+            <h5 id="drawer-new-policie-label"
+                class="mb-4 inline-flex items-center text-base font-semibold text-zinc-500 dark:text-zinc-400">
+                Nueva política
+            </h5>
+            <button type="button" data-drawer="#drawer-new-policie"
+                class="close-drawer absolute end-2.5 top-2.5 inline-flex h-8 w-8 items-center justify-center rounded-lg bg-transparent text-sm text-zinc-400 hover:bg-zinc-200 hover:text-zinc-900 dark:hover:bg-zinc-900 dark:hover:text-white">
+                <svg class="h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                </svg>
+                <span class="sr-only">Close menu</span>
+            </button>
+            <div>
+                <form action="{{ route('admin.policies.store') }}" class="flex flex-col gap-4"
+                    enctype="multipart/form-data" method="POST" id="formAddCategorie">
+                    @csrf
+                    <div class="w-full">
+                        <x-input type="text" name="name" placeholder="Ingresa el nombre de la política"
+                            label="Nombre" value="{{ old('name') }}" />
+                    </div>
+                    <div>
+                        <label class="mb-2 block text-sm font-medium text-zinc-900 dark:text-white">
+                            Archivo
+                        </label>
+                        <div class="flex w-full items-center justify-center">
+                            <label for="file-policie"
+                                class="dark:hover:bg-bray-800 @error('path') is-invalid  @enderror flex w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-zinc-300 bg-zinc-50 p-4 hover:bg-zinc-100 dark:border-zinc-600 dark:bg-transparent dark:hover:border-zinc-500 dark:hover:bg-zinc-950">
+                                <p>
+                                    <span
+                                        class="flex items-center justify-center gap-4 text-sm text-zinc-500 dark:text-zinc-400">
+                                        <x-icon icon="cloud-upload" class="h-6 w-6" />
+                                        Selecciona un archivo
+                                    </span>
+                                </p>
+                                <input id="file-policie" type="file" class="hidden" name="file_path"
+                                    accept="application/pdf" />
+                            </label>
+                        </div>
+                        @error('image')
+                            <span class="text-sm text-red-500">{{ $message }}</span>
+                        @enderror
+
+                        <!-- Contenedor para la vista previa del PDF -->
+                        <div id="pdf-preview" class="mt-4 hidden h-80 w-full">
+                            <iframe id="pdf-frame" class="h-full w-full" frameborder="0"></iframe>
+                        </div>
+                    </div>
+
+                    <div class="flex items-center justify-center gap-2">
+                        <x-button type="submit" text="Agregar política" icon="add-circle" typeButton="primary" />
+                        <x-button type="button" data-drawer="#drawer-new-policie" class="close-drawer" text="Cancelar"
+                            typeButton="secondary" />
+                    </div>
+                </form>
+            </div>
+        </div>
+
     </div>
 @endsection
