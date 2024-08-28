cb12d4ec69903e630c1e6b6cfc5ccc46cb96547e
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-27 22:30:36 -0600

    Se agrego la funcionalidad de cambiar el modo de mantenimiento de la aplicación

--- app/Http/Controllers/SettingsGeneralController.php
+++ app/Http/Controllers/SettingsGeneralController.php
@@ -2,12 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Settings;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SettingsGeneralController extends Controller
 {
     public function index()
     {
-        return view('admin.general_settings.index');
+        $maintenance = Settings::where('key', 'site_in_maintenance')->first();
+        return view('admin.general_settings.index', compact('maintenance'));
+    }
+
+    public function maintenanceUpdate(Request $request)
+    {
+        $maintenance = $request->input("site_in_maintenance");
+        DB::beginTransaction();
+        try {
+            if (!$request->input("site_in_maintenance")) {
+                $maintenance = 0;
+            } else {
+                $maintenance = 1;
+            }
+            $settings = Settings::where('key', 'site_in_maintenance')->first();
+            $settings->update(['value' => $maintenance]);
+            DB::commit();
+            return response()->json(['success' => 'Maintenance mode updated successfully']);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json(['error' => 'Error updating maintenance mode']);
+        }
     }
 }
