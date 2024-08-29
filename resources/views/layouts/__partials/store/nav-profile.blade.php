1a7e439e97dbdc35a1178d9326dff824d3b94a60
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-29 15:14:55 -0600

    Se actualizo el navbar del perfil de la tienda

--- resources/views/layouts/__partials/store/nav-profile.blade.php
+++ resources/views/layouts/__partials/store/nav-profile.blade.php
@@ -1,8 +1,8 @@
 <div class="flex flex-col">
     <h2 class="px-4 pt-4 font-primary text-2xl font-bold text-secondary">Mi cuenta</h2>
     <div class="mb-4 flex flex-col">
-        <a href="{{ Route('account') }}"
-            class="link-profile {{ Route::is('account') ? 'active' : '' }} relative flex items-center p-2 ps-4 text-secondary">
+        <a href="{{ Route('account.index') }}"
+            class="link-profile {{ Route::is('account.index') ? 'active' : '' }} relative flex items-center p-2 ps-4 text-secondary">
             General
         </a>
         <a href="{{ Route('orders') }}"
@@ -17,8 +17,9 @@
             class="link-profile {{ Route::is('account.settings') ? 'active' : '' }} relative flex items-center p-2 ps-4 text-secondary">
             Configuración
         </a>
-        <a href="#" class="link-profile relative flex items-center p-2 ps-4 text-secondary">
-            Dirección de envío
+        <a href="{{ Route('account.addresses.index') }}"
+            class="{{ Route::is('account.addresses.index') ? 'active' : '' }} link-profile relative flex items-center p-2 ps-4 text-secondary">
+            Direcciones
         </a>
         <a href="#" class="link-profile relative flex items-center p-2 ps-4 text-secondary">Mensajes</a>
     </div>
