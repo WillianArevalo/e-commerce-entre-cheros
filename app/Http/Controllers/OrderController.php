cd9fd33234be427ace4cd0e80894fd430202059a
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-29 15:09:59 -0600

    Se actualizo el controlador de las ordenes

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\InfoOrderRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -15,5 +17,32 @@ class OrderController extends Controller
     public function index()
     {
         return view("admin.orders.index");
+    }
+
+    public function addInfoCustomer(InfoOrderRequest $request)
+    {
+        $validated = $request->validated();
+        $validated["type"] = "shipping_address";
+        $user = User::find(auth()->id());
+
+        if ($user) {
+            $user->update($validated);
+            if ($user->customer) {
+                $user->customer->update($validated);
+                $customer = $user->customer;
+            } else {
+                $customer = $user->customer()->create($validated);
+            }
+
+            if ($customer) {
+                if ($customer->address) {
+                    $customer->address->update($validated);
+                } else {
+                    $address = $customer->address()->create($validated);
+                }
+                return redirect()->route("checkout")->with("success", "Datos editados correctamente");
+            }
+        }
+        return back()->with("error", "Error al crear o actualizar el cliente");
     }
 }
