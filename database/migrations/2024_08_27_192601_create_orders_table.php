b28849003a8e9b0affa892d71ae20a133ad8e6b9
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-08-29 15:20:08 -0600

    Se agrego la migracion de la tabla orders y order_items

---
+++ database/migrations/2024_08_27_192601_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string("number_order");
+            $table->string("status")->default("pending");
+            $table->decimal("total", 10, 2);
+            $table->foreignId('customer_id')->constrained();
+            $table->foreignId("currency_id")->constrained();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('shipping_method_id')->constrained();
+            $table->foreignId('payment_method_id')->constrained();
+            $table->foreignId('address_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
