92a9f5e30fa33bbe5bbd01bb52fd675755a91dea
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-09-04 14:03:29 -0600

    Se actualizo el seeder de los settings generales

--- database/seeders/SettingsSeeder.php
+++ database/seeders/SettingsSeeder.php
@@ -3,8 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Settings;
-use Hamcrest\Core\Set;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class SettingsSeeder extends Seeder
