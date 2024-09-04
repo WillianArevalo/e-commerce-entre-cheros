f077230df837c190317fd188ef6128747522523d
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-09-04 13:58:53 -0600

    Se actualizo el archivo tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 export default {
     content: [
         "./resources/**/*.blade.php",
@@ -9,45 +10,25 @@ export default {
     theme: {
         extend: {
             colors: {
-                primary: "var(--primary-color)",
-                secondary: "var(--secondary-color)",
-                tertiary: "var(--tertiary-color)",
-
-                "primary-store": "var(--primary-color-store)",
-                "secondary-store": "var(--secondary-color-store)",
-                "tertiary-store": "var(--tertiary-color-store)",
+                primary: {
+                    DEFAULT: "var(--primary-color)", // Usamos la variable CSS como valor por defecto
+                    50: "var(--primary-color-50)",
+                    100: "var(--primary-color-100)",
+                    200: "var(--primary-color-200)",
+                    300: "var(--primary-color-300)",
+                    400: "var(--primary-color-400)",
+                    500: "var(--primary-color-500)",
+                    600: "var(--primary-color-600)",
+                    700: "var(--primary-color-700)",
+                    800: "var(--primary-color-800)",
+                    900: "var(--primary-color-900)",
+                    950: "var(--primary-color-950)",
+                },
             },
             fontFamily: {
                 "league-spartan": ["League Spartan", "sans-serif"],
                 poppins: ["Poppins", "sans-serif"],
                 mystical: ["Mystical", "sans-serif"],
-            },
-            borderColor: {
-                primary: "var(--primary-color)",
-                secondary: "var(--secondary-color)",
-                tertiary: "var(--tertiary-color)",
-
-                "primary-store": "var(--primary-color-store)",
-                "secondary-store": "var(--secondary-color-store)",
-                "tertiary-store": "var(--tertiary-color-store)",
-            },
-            textColor: {
-                primary: "var(--primary-color)",
-                secondary: "var(--secondary-color)",
-                tertiary: "var(--tertiary-color)",
-
-                "primary-store": "var(--primary-color-store)",
-                "secondary-store": "var(--secondary-color-store)",
-                "tertiary-store": "var(--tertiary-color-store)",
-            },
-            backgroundColor: {
-                primary: "var(--primary-color)",
-                secondary: "var(--secondary-color)",
-                tertiary: "var(--tertiary-color)",
-
-                "primary-store": "var(--primary-color-store)",
-                "secondary-store": "var(--secondary-color-store)",
-                "tertiary-store": "var(--tertiary-color-store)",
             },
         },
     },
