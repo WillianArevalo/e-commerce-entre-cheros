6aec79ccd15eb1c6212a3f1d4d8c179e7f92e95b
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-09-04 13:59:18 -0600

    Se actualizo la vista del componente de badge status

--- resources/views/components/badge-status.blade.php
+++ resources/views/components/badge-status.blade.php
@@ -1,11 +1,11 @@
  @if ($status === 1)
      <span
-         class="rounded-full border-2 border-green-300 bg-green-200 px-4 py-1 font-secondary text-xs font-medium text-green-800 dark:border-green-400 dark:bg-green-800 dark:text-green-100">
+         class="inline-block rounded-full bg-green-100 px-2 py-1 text-xs font-semibold uppercase text-green-500 dark:bg-green-900 dark:text-green-300"">
          Activo
      </span>
  @else
      <span
-         class="rounded-full border-2 border-red-300 bg-red-200 px-4 py-1 font-secondary text-xs font-medium text-red-800 dark:border-red-400 dark:bg-red-800 dark:text-red-100">
+         class="inline-block rounded-full bg-red-100 px-2 py-1 text-xs font-semibold uppercase text-red-500 dark:bg-red-900 dark:text-red-300"">
          Inactivo
      </span>
  @endif
