3f9368305d7bf6e835a6ca5384ca0d04ed921343
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-09-04 14:04:37 -0600

    Se agrego el partial del encabezado de cada página del administrador

--- resources/views/layouts/__partials/admin/header-crud-page.blade.php
+++ resources/views/layouts/__partials/admin/header-crud-page.blade.php
@@ -1,10 +1,11 @@
-<div class="flex w-full flex-col items-start border-y bg-white px-4 py-4 dark:border-zinc-800 dark:bg-black">
+<div
+    class="flex w-full flex-col items-start border-y border-zinc-400 bg-white px-4 py-4 dark:border-zinc-800 dark:bg-black">
     <a href="{{ $url }}"
         class="flex items-center justify-center gap-1 text-sm text-zinc-500 hover:text-zinc-600 hover:underline dark:text-zinc-400">
-        <x-icon icon="arrow-left-02" class="h-4 w-4 text-current" />
+        <x-icon icon="return" class="h-4 w-4 text-current" />
         {{ $text }}
     </a>
-    <h1 class="font-secondary text-3xl font-bold text-secondary dark:text-blue-400">
+    <h1 class="font-secondary text-3xl font-bold text-primary-600">
         {{ $title }}
     </h1>
 </div>
