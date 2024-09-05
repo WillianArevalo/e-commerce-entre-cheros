17d794939b27acdd7bb3376437e2761915db3ca1
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-09-04 18:00:40 -0600

    Se actualizo el estilo del breadcrumb

--- resources/views/layouts/__partials/admin/breadcrumb.blade.php
+++ resources/views/layouts/__partials/admin/breadcrumb.blade.php
@@ -1,25 +1,28 @@
 <!-- Breadcrumb -->
 @php $breadcrumbs = Breadcrumbs::generate(); @endphp
 @if (!empty($breadcrumbs))
-    <div class="flex justify-end">
-        <nav class="flex w-max px-6 py-2 text-zinc-700" aria-label="Breadcrumb">
+    <div class="flex items-center justify-end">
+        <nav class="flex w-max px-6 py-3 text-zinc-700" aria-label="Breadcrumb">
             <ol class="inline-flex items-center space-x-1 uppercase rtl:space-x-reverse md:space-x-2">
                 @foreach ($breadcrumbs as $key => $breadcrumb)
                     @if ($breadcrumb->url && !$loop->last)
                         <li>
                             <div class="flex items-center gap-4">
                                 <a href="{{ $breadcrumb->url }}"
-                                    class="flex items-center gap-1 rounded-lg px-3 py-1 text-sm font-medium text-zinc-700 hover:text-blue-600 dark:text-zinc-400 dark:hover:bg-zinc-950 dark:hover:text-white">
-                                    {{ $breadcrumb->title }}
+                                    class="flex items-center gap-1 rounded-lg px-2 py-1 text-xs font-semibold text-zinc-500 hover:bg-primary-50 hover:text-primary-500 dark:text-zinc-400 dark:hover:bg-primary-950 dark:hover:text-primary-300">
+                                    {!! $breadcrumb->title !!}
                                 </a>
-                                <x-icon icon="arrow-right" class="mx-1 block h-5 w-5 text-zinc-400 rtl:rotate-180" />
+                                <x-icon icon="arrow-badge-right"
+                                    class="mx-1 block h-5 w-5 text-zinc-400 rtl:rotate-180" />
                             </div>
                         </li>
                     @else
                         <li aria-current="page">
                             <div class="flex items-center">
                                 <span
-                                    class="ms-1 text-sm font-medium text-zinc-500 dark:text-zinc-400 md:ms-2">{{ $breadcrumb->title }}</span>
+                                    class="ms-1 flex items-center gap-1 rounded-lg bg-primary-50 px-2 py-1 text-xs font-semibold text-primary-500 dark:bg-primary-950 dark:text-primary-300 md:ms-2">
+                                    {!! $breadcrumb->title !!}
+                                </span>
                             </div>
                         </li>
                     @endif
