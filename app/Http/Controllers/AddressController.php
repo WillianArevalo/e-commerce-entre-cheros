7ab8a12284996815ca52c43545c1fcdddd7c22c8
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-09-08 15:47:41 -0600

    Se actualizo el controlador de las direcciones

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -11,6 +11,92 @@ use Illuminate\Support\Facades\DB;
 
 class AddressController extends Controller
 {
+    /** Admin */
+    public function store(AddressRequest $request)
+    {
+        $validated = $request->validated();
+        $customer = Customer::find($validated["customer_id"]);
+        if (!isset($validated["default"])) $validated["default"] = 0;
+        if (!isset($validated["active"])) $validated["active"] = 0;
+        if (!$customer) return redirect()->route("admin.customers.index")->with("error", "Cliente no encontrado");
+
+        $existingAddress = Address::where("customer_id", $customer->id)
+            ->where("type", $validated["type"])
+            ->first();
+
+        if ($existingAddress) {
+            return redirect()->route("admin.customers.edit", $customer->id)->with("error", "Ya existe una dirección con el mismo tipo");
+        }
+
+        DB::beginTransaction();
+        try {
+            $address = new Address();
+            $address->fill($validated);
+            $address->customer_id = $customer->id;
+            $address->save();
+            DB::commit();
+            return redirect()->route("admin.customers.edit",  $customer->id)->with("success", "Dirección guardada correctamente");
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->route("admin.customers.edit", $customer->id)->with("error", "Error al guardar la dirección. Error: " . $e->getMessage());
+        }
+    }
+
+    public function destroy(string $id)
+    {
+        $address = Address::find($id);
+        if (!$address) return redirect()->route("admin.customers.index")->with("error", "Dirección no encontrada");
+        DB::beginTransaction();
+        try {
+            $address->delete();
+            DB::commit();
+            return redirect()->route("admin.customers.edit", $address->customer_id)->with("success", "Dirección eliminada correctamente");
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->route("admin.customers.edit", $address->customer_id)->with("error", "Error al eliminar la dirección. Error: " . $e->getMessage());
+        }
+    }
+
+    public function edit(string $id)
+    {
+        $address = Address::find($id);
+        if (!$address) return response()->json(["error" => "Dirección no encontrada"], 404);
+        $addresses = Addresses::getAddresses();
+        return response()->json(["success" => "Dirección encontrada", "address" => $address]);
+    }
+
+    public function update(AddressRequest $request, string $id)
+    {
+        $validated = $request->validated();
+        $address = Address::find($id);
+        if (!isset($validated["default"])) $validated["default"] = 0;
+        if (!isset($validated["active"])) $validated["active"] = 0;
+        if (!$address) return redirect()->route("admin.customers.index")->with("error", "Dirección no encontrada");
+
+        $existingAddress = Address::where("customer_id", $address->customer_id)
+            ->where("type", $validated["type"])
+            ->where("id", "!=", $address->id)
+            ->first();
+
+        if ($existingAddress) {
+            return redirect()->route("admin.customers.edit", $address->customer_id)->with("error", "Ya existe una dirección con el mismo tipo");
+        }
+
+        DB::beginTransaction();
+        try {
+            $address->fill($validated);
+            $address->save();
+            DB::commit();
+            return redirect()->route("admin.customers.edit", $address->customer_id)->with("success", "Dirección actualizada correctamente");
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->route("admin.customers.edit", $address->customer_id)->with("error", "Error al actualizar la dirección. Error: " . $e->getMessage());
+        }
+    }
+
+
+    /** Store   */
+
     /**
      * Display a listing of the resource.
      */
@@ -34,10 +120,10 @@ class AddressController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(AddressRequest $request)
+    public function storeAccount(AddressRequest $request)
     {
         $validated = $request->validated();
-        $user = auth()->user();
+        $user =  auth()->user();
         $customer = Customer::where("user_id", $user->id)->first();
         if (!$customer) return redirect()->route("account.addresses.index")->with("error", "Cliente no encontrado");
 
@@ -74,7 +160,7 @@ class AddressController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $slug)
+    public function editAccount(string $slug)
     {
         $address = Address::where("slug", $slug)->first();
         if (!$address) return redirect()->route("account.addresses.index")->with("error", "Dirección no encontrada");
@@ -85,10 +171,11 @@ class AddressController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(AddressRequest $request, string $id)
+    public function updateAccount(AddressRequest $request, string $id)
     {
         $validated = $request->validated();
         $address = Address::find($id);
+
         if (!$address) return redirect()->route("account.addresses.index")->with("error", "Dirección no encontrada");
 
         $existingAddress = Address::where("customer_id", $address->customer_id)
@@ -115,7 +202,7 @@ class AddressController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroyAccount(string $id)
     {
         $address = Address::find($id);
         if (!$address) return redirect()->route("account.addresses.index")->with("error", "Dirección no encontrada");
