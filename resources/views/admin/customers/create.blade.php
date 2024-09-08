f5b7b166e8071f0d748f38ca7d9aa3b19789968b
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-09-08 15:56:33 -0600

    Se actualizaron las vistas de los clientes del admin

--- resources/views/admin/customers/create.blade.php
+++ resources/views/admin/customers/create.blade.php
@@ -1,9 +1,7 @@
 @extends('layouts.admin-template')
-
-@section('title', 'Nuevo producto')
-
+@section('title', 'Nuevo cliente')
 @section('content')
-    <div class="mt-4">
+    <div>
         @include('layouts.__partials.admin.header-crud-page', [
             'title' => 'Nuevo cliente',
             'text' => 'Regresar a la lista de clientes',
@@ -11,22 +9,33 @@
         ])
         <div class="bg-white p-4 dark:bg-black">
             <div class="mx-auto w-full">
-                <form action="{{ route('admin.customers.store') }}" class="flex flex-col gap-4" enctype="multipart/form-data"
-                    method="POST">
-                    @csrf
-                    <div class="flex flex-col gap-1">
-                        <h2 class="text-lg uppercase text-zinc-700 dark:text-zinc-300">Información del cliente</h2>
-                        <p class="text-sm text-zinc-500 dark:text-zinc-400">
-                            Los campos marcados con <span class="text-red-500">*</span> son obligatorios
-                        </p>
-                    </div>
-                    <div class="flex gap-4">
-                        <div class="flex flex-[2] flex-col gap-4">
+                <div class="flex flex-col gap-1">
+                    <h2 class="text-lg font-bold uppercase text-zinc-700 dark:text-zinc-300">Información del cliente</h2>
+                    <p class="text-sm text-zinc-500 dark:text-zinc-400">
+                        Los campos marcados con <span class="text-red-500">*</span> son obligatorios
+                    </p>
+                </div>
+                <div class="mt-4 flex flex-col">
+                    <form action="{{ route('admin.customers.store') }}" class="flex flex-col gap-4"
+                        enctype="multipart/form-data" method="POST">
+                        @csrf
+                        <div class="flex flex-1 flex-col gap-4">
+                            <div
+                                class="flex flex-col gap-4 rounded-lg border border-zinc-400 bg-white p-4 dark:border-zinc-800 dark:bg-black">
+                                <div class="w-3/12">
+                                    <x-select label="Usuario" text="Seleccionar usuario existente" id="user_id"
+                                        name="user_id" required :options="$users->pluck('email', 'id')->toArray()" />
+                                </div>
+                                <div class="w-max">
+                                    <x-button type="a" href="{{ Route('admin.users.create') }}" icon="user-plus"
+                                        text="Crear usuario" typeButton="secondary" />
+                                </div>
+                            </div>
                             <div class="rounded-lg border border-zinc-400 bg-white p-4 dark:border-zinc-800 dark:bg-black">
-                                <h2 class="mb-2 text-base font-semibold text-blue-700 dark:text-blue-400">
+                                <h2 class="mb-2 text-base font-semibold text-black dark:text-white">
                                     Información general
                                 </h2>
-                                <div class="flex flex-col gap-4">
+                                <div class="flex gap-4">
                                     <div class="flex-1">
                                         <x-input label="Nombre" type="text" id="name" name="name"
                                             placeholder="Ingresa el nombre del cliente" value="{{ old('name') }}"
@@ -40,13 +49,19 @@
                                 </div>
                                 <div class="mt-4 flex gap-4">
                                     <div class="flex-1">
-                                        <x-input label="Fecha de nacimiento" type="date" id="birthdate" name="birthdate"
-                                            value="{{ old('birthdate') }}" required />
+                                        <x-input icon="calendar" label="Fecha de nacimiento" type="date" id="birthdate"
+                                            name="birthdate" value="{{ old('birthdate') }}" required />
                                     </div>
-                                    <div class="flex-1">
-                                        <x-input label="Telefono" type="text" id="phone" name="phone"
-                                            value="{{ old('phone') }}" placeholder="Ingresa el telefono del cliente"
-                                            required />
+                                    <div class="flex flex-[2] gap-4">
+                                        <div class="flex-1">
+                                            <x-input label="Código de área" type="text" icon="plus" name="area_code"
+                                                value="{{ old('area_code') }}" placeholder="503" required />
+                                        </div>
+                                        <div class="flex-[2]">
+                                            <x-input label="Telefono" type="text" id="phone" name="phone"
+                                                value="{{ old('phone') }}" icon="phone"
+                                                placeholder="Ingresa el telefono del cliente" required />
+                                        </div>
                                     </div>
                                     <div class="w-max flex-1">
                                         <x-select label="Sexo" id="gender" name="gender" value="male" selected=""
@@ -55,25 +70,31 @@
                                 </div>
                             </div>
                             <div class="rounded-lg border border-zinc-400 bg-white p-4 dark:border-zinc-800 dark:bg-black">
-                                <h2 class="mb-2 text-base font-semibold text-blue-700 dark:text-blue-400">
-                                    Información de contacto
+                                <h2 class="mb-2 text-base font-semibold text-black dark:text-white">
+                                    Dirección
                                 </h2>
                                 <div class="flex flex-col gap-4">
-                                    <div class="flex-1">
-                                        <x-input label="Dirección (línea 1)" type="text" id="address_line_1"
-                                            name="address_line_1" placeholder="Ingresa la primera línea de la dirección"
-                                            required />
+                                    <div class="flex flex-1 gap-4">
+                                        <div class="flex-1">
+                                            <x-select label="Tipo de dirección" id="address_type" name="type" required
+                                                :options="$addresses" />
+                                        </div>
+                                        <div class="flex-[3]">
+                                            <x-input label="Dirección (línea 1)" type="text" id="address_line_1"
+                                                name="address_line_1" icon="location"
+                                                placeholder="Ingresa la primera línea de la dirección" required />
+                                        </div>
                                     </div>
                                     <div class="flex-1">
                                         <x-input label="Dirección (línea 2)" type="text" id="address_line_2"
-                                            name="address_line_2" placeholder="Ingresa la segunda línea de la dirección"
-                                            required />
+                                            name="address_line_2" icon="location"
+                                            placeholder="Ingresa la segunda línea de la dirección" required />
                                     </div>
                                 </div>
                                 <div class="mt-4 flex gap-4">
                                     <div class="flex-[2]">
-                                        <x-input type="text" label="País" id="country" name="country"
-                                            placeholder="Ingresa el país" required />
+                                        <x-select type="text" label="País" id="country" name="country"
+                                            :options="$countries" required />
                                     </div>
                                     <div class="w-max flex-[2]">
                                         <x-input type="text" label="Estado" id="state" name="state"
@@ -85,85 +106,31 @@
                                     </div>
                                 </div>
                                 <div class="mt-4 flex gap-4">
-                                    <div class="w-max flex-1">
+                                    <div class="w-1/2">
                                         <x-input type="text" label="Ciudad" id="city" name="city"
                                             placeholder="Ingresa la ciudad del país" required />
                                     </div>
-                                    <div class="flex-1">
-                                        <x-input label="Código de área" type="text" id="area_code" name="area_code"
-                                            placeholder="Ingresa el código del área" required />
+                                </div>
+                                <div class="mt-4 flex gap-10">
+                                    <div>
+                                        <x-input type="checkbox" label="Dirección por defecto" id="default"
+                                            name="default" />
+                                    </div>
+                                    <div>
+                                        <x-input type="checkbox" label="Activa" id="status" name="status" />
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="flex-1">
-                            <div class="rounded-lg border border-zinc-400 bg-white p-4 dark:border-zinc-800 dark:bg-black">
-                                <h2 class="mb-2 text-base font-semibold text-blue-700 dark:text-blue-400">
-                                    Datos de usuario
-                                </h2>
-                                <div>
-                                    <div class="flex items-center justify-between">
-                                        <p class="p-4 text-sm font-medium text-zinc-600 dark:text-zinc-400">
-                                            Foto de perfil
-                                        </p>
-                                        <div class="flex justify-end">
-                                            <label for="profile"
-                                                class="m-4 flex cursor-pointer items-center gap-2 rounded border-2 border-zinc-400 px-3.5 py-2.5 text-sm font-medium text-zinc-600 transition-colors hover:bg-zinc-100 dark:border-zinc-800 dark:text-white dark:hover:bg-zinc-900">
-                                                <x-icon icon="image-add" class="h-5 w-5 text-current" />
-                                                Agregar foto
-                                            </label>
-                                            <input type="file" name="profile" id="profile" class="hidden">
-                                        </div>
-                                    </div>
-                                    <div class="flex flex-col items-center justify-center py-4">
-                                        <img src="{{ asset('images/default-profile.png') }}" alt="Foto de perfil"
-                                            id="previewImage"
-                                            class="@error('profile') is-invalid @enderror h-40 w-40 rounded-full object-cover">
-                                        @error('profile')
-                                            <span class="mt-4 text-sm text-red-500">
-                                                {{ $message }}
-                                            </span>
-                                        @enderror
-                                    </div>
-                                    <div class="flex flex-col gap-4">
-                                        <div class="flex-1">
-                                            <x-input label="Nombre de usuario" type="text" id="username"
-                                                name="username" placeholder="Ingresa el nombre del usuario"
-                                                value="{{ old('username') }}" required />
-                                        </div>
-                                        <div class="flex-1">
-                                            <x-input label="Correo electronico" type="email" id="email_customer"
-                                                name="email" placeholder="Ingresa el correo electronico del cliente"
-                                                value="{{ old('email') }}" required />
-                                        </div>
-                                    </div>
-                                    <div class="mt-4 flex flex-col gap-4">
-                                        <div class="flex-1">
-                                            <x-input label="Contraseña" type="password" id="password_customer"
-                                                name="password" placeholder="Ingresa la contraseña del cliente"
-                                                required />
-                                        </div>
-                                        <div class="flex-1">
-                                            <x-input label="Confirmar contraseña" type="password"
-                                                id="password_confirmation" name="password_confirmation"
-                                                placeholder="Confirmar contraseña" required />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                        <div class="flex items-center justify-center gap-2">
+                            <x-button type="submit" text="Agregar cliente" icon="plus" typeButton="primary" />
+                            <x-button type="a" href="{{ url()->previous() }}" text="Regresar"
+                                typeButton="secondary" />
                         </div>
-                    </div>
-                    <div class="flex items-center justify-center gap-2">
-                        <x-button type="submit" text="Agregar cliente" icon="plus" typeButton="primary" />
-                        <x-button type="a" href="{{ route('admin.customers.index') }}" text="Regresar"
-                            typeButton="secondary" />
-                    </div>
-                </form>
-                <form action="{{ route('admin.subcategories.search') }}" id="formSearchSubcategorie" method="POST">
-                    @csrf
-                    <input type="hidden" name="categorie_id" id="categorieIdSearch">
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
+    </div>
     </div>
 @endsection
--- resources/views/admin/customers/index.blade.php
+++ resources/views/admin/customers/index.blade.php
@@ -1,16 +1,14 @@
 @extends('layouts.admin-template')
-
-@section('title', 'Categorías')
-
+@section('title', 'Clientes')
 @section('content')
-    <div class="mt-4 rounded-lg">
+    <div>
         @include('layouts.__partials.admin.header-page', [
             'title' => 'Clientes',
             'description' => 'Administrar clientes registrados.',
         ])
-        <div class="bg-zinc-50 p-4 dark:bg-black">
+        <div class="bg-zinc-50 dark:bg-black">
             <div class="mx-auto w-full">
-                <div class="relative bg-white shadow-md dark:border dark:border-zinc-800 dark:bg-black sm:rounded-lg">
+                <div class="relative bg-white dark:bg-black">
                     <div class="border-b border-zinc-400 p-4 dark:border-zinc-800">
                         <h2 class="text-base font-semibold text-zinc-700 dark:text-zinc-200">
                             Lista de clientes registrados
@@ -66,73 +64,90 @@
                             </div>
                         </div>
                     </div>
-                    <div>
-                        <table class="w-full text-left text-sm text-zinc-500 dark:text-zinc-400">
-                            <thead class="bg-zinc-50 text-xs uppercase text-zinc-700 dark:bg-zinc-900 dark:text-zinc-300">
-                                <tr>
-                                    <th scope="col" class="px-4 py-3">Foto</th>
-                                    <th scope="col" class="px-4 py-3">Nombre</th>
-                                    <th scope="col" class="px-4 py-3">Apellido</th>
-                                    <th scope="col" class="px-4 py-3">Correo</th>
-                                    <th scope="col" class="px-4 py-3">Telefono</th>
-                                    <th scope="col" class="px-4 py-3">Estado</th>
-                                    <th scope="col" class="px-4 py-3">Acciones</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($customers as $customer)
-                                    <tr>
-                                        <td class="px-4 py-3">
-                                            <img src="{{ Storage::url($customer->user->profile) }}"
-                                                alt="Foto de perfil del usuario {{ $customer->user->username }}"
-                                                class="h-10 w-10 rounded-full object-cover">
-                                        </td>
-                                        <td class="px-4 py-3">
-                                            <span>{{ $customer->user->name }}</span>
-                                        </td>
-                                        <td class="px-4 py-3">
-                                            <span>{{ $customer->user->last_name }}</span>
-                                        </td>
-                                        <td class="px-4 py-3">
-                                            <span>{{ $customer->user->email }}</span>
-                                        </td>
-                                        <td class="px-4 py-3">
-                                            <span>{{ $customer->phone }}</span>
-                                        </td>
-                                        <td class="px-4 py-3">
-                                            <span
-                                                class="me-2 rounded-full bg-blue-100 px-3 py-1 font-medium text-blue-800 dark:bg-blue-900 dark:text-blue-300">
-                                                {{ Str::ucfirst($customer->status) }}
+                    <div class="mx-4">
+                        <x-table>
+                            <x-slot name="thead">
+                                <x-tr>
+                                    <x-th class="w-10">
+                                        <input id="default-checkbox" type="checkbox" value=""
+                                            class="h-4 w-4 rounded border-2 border-zinc-400 bg-zinc-100 text-primary-600 focus:ring-2 focus:ring-primary-500 dark:border-zinc-800 dark:bg-zinc-950 dark:ring-offset-zinc-800 dark:focus:ring-primary-600">
+                                    </x-th>
+                                    <x-th>Foto</x-th>
+                                    <x-th>Nombre</x-th>
+                                    <x-th>Apellido</x-th>
+                                    <x-th>Correo</x-th>
+                                    <x-th>Telefono</x-th>
+                                    <x-th>Estado</x-th>
+                                    <x-th :last="true">Acciones</x-th>
+                                </x-tr>
+                            </x-slot>
+                            <x-slot name="tbody">
+                                @if ($customers->isEmpty())
+                                    <x-tr section="body" class="text-center">
+                                        <x-td colspan="8" class="py-8">
+                                            <span class="text-sm font-medium text-zinc-900 dark:text-zinc-100">
+                                                No se ha agregado ningún cliente
                                             </span>
-                                        </td>
-                                        <td class="px-4 py-3">
-                                            <div class="flex gap-2">
-                                                <x-button type="a"
-                                                    href="{{ route('admin.customers.edit', $customer->id) }}"
-                                                    icon="edit" typeButton="success" onlyIcon="true" />
-                                                <form action="{{ route('admin.customers.destroy', $customer->id) }}"
-                                                    id="formDeleteCustomer-{{ $customer->id }}" method="POST">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <x-button type="button"
-                                                        data-form="formDeleteCustomer-{{ $customer->id }}" icon="delete"
-                                                        typeButton="danger" class="buttonDelete" onlyIcon="true"
-                                                        data-modal-target="deleteModal" data-modal-toggle="deleteModal" />
-                                                </form>
-                                                <x-button type="a"
-                                                    href="{{ route('admin.customers.edit', $customer->id) }}"
-                                                    icon="view" typeButton="secondary" onlyIcon="true" />
-                                            </div>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                                        </x-td>
+                                    </x-tr>
+                                @else
+                                    @foreach ($customers as $customer)
+                                        <x-tr section="body">
+                                            <x-td>
+                                                <input id="default-checkbox" type="checkbox" value="{{ $customer->id }}"
+                                                    class="h-4 w-4 rounded border-2 border-zinc-400 bg-zinc-100 text-primary-600 focus:ring-2 focus:ring-primary-500 dark:border-zinc-800 dark:bg-zinc-950 dark:ring-offset-zinc-800 dark:focus:ring-primary-600">
+                                            </x-td>
+                                            <x-td>
+                                                <img src="{{ Storage::url($customer->user->profile) }}"
+                                                    alt="Foto de perfil del usuario {{ $customer->user->username }}"
+                                                    class="h-10 w-10 rounded-full object-cover">
+                                            </x-td>
+                                            <x-td>
+                                                <span>{{ $customer->user->name }}</span>
+                                            </x-td>
+                                            <x-td>
+                                                <span>{{ $customer->user->last_name }}</span>
+                                            </x-td>
+                                            <x-td>
+                                                <span>{{ $customer->user->email }}</span>
+                                            </x-td>
+                                            <x-td>
+                                                <span>{{ $customer->phone }}</span>
+                                            </x-td>
+                                            <x-td>
+                                                <x-badge-status :status="$customer->status" />
+                                            </x-td>
+                                            <x-td>
+                                                <div class="flex gap-2">
+                                                    <x-button type="a"
+                                                        href="{{ route('admin.customers.edit', $customer->id) }}"
+                                                        icon="edit" typeButton="secondary" onlyIcon="true" />
+                                                    <form action="{{ route('admin.customers.destroy', $customer->id) }}"
+                                                        id="formDeleteCustomer-{{ $customer->id }}" method="POST">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <x-button type="button"
+                                                            data-form="formDeleteCustomer-{{ $customer->id }}"
+                                                            icon="delete" typeButton="secondary" class="buttonDelete"
+                                                            onlyIcon="true" data-modal-target="deleteModal"
+                                                            data-modal-toggle="deleteModal" />
+                                                    </form>
+                                                    <x-button type="a"
+                                                        href="{{ route('admin.customers.edit', $customer->id) }}"
+                                                        icon="view" typeButton="secondary" onlyIcon="true" />
+                                                </div>
+                                            </x-td>
+                                        </x-tr>
+                                    @endforeach
+                                @endif
+                            </x-slot>
+                        </x-table>
                     </div>
                 </div>
             </div>
         </div>
         <x-delete-modal modalId="deleteModal" title="¿Estás seguro de eliminar el cliente?"
-            message="No podrás recuperar este registro" action="" />
+            message="Se eliminará el usuario y toda la información relacionada a el cliente. No podrás recuperar este registro"
+            action="" />
     </div>
 @endsection
