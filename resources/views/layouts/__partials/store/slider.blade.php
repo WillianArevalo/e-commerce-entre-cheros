53394078db92e74a6cb315ab059e3bdad0e631ab
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-09-14 10:27:57 -0600

    Se actualizo el slider de la tienda

--- resources/views/layouts/__partials/store/slider.blade.php
+++ resources/views/layouts/__partials/store/slider.blade.php
@@ -1,8 +1,8 @@
   @if ($products->count() > 3)
-      <div class="relative mx-auto mt-4 flex w-full flex-wrap items-center justify-center">
+      <div class="relative mx-auto mt-4 flex w-[91%] flex-wrap items-center justify-center">
           <button
               class="button-prev absolute -left-14 z-30 flex cursor-pointer items-center justify-center rounded-full border border-zinc-400 p-2 hover:bg-zinc-100">
-              <x-icon icon="arrow-left" class="h-6 w-6 text-secondary" />
+              <x-icon-store icon="arrow-left" class="h-6 w-6 text-secondary" />
           </button>
           <div class="swiper mySwiper">
               <div class="swiper-wrapper">
@@ -13,7 +13,7 @@
           </div>
           <button
               class="button-next absolute -right-14 z-30 flex cursor-pointer items-center justify-center rounded-full border border-zinc-400 p-2 hover:bg-zinc-100">
-              <x-icon icon="arrow-right" class="h-6 w-6 text-secondary" />
+              <x-icon-store icon="arrow-right" class="h-6 w-6 text-secondary" />
           </button>
       </div>
   @else
