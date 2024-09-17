fe5d1ae0eda36bd4987669330653dd23bf265f22
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-09-16 22:53:13 -0600

    Se actualizaron los controladores del review, se agrego el review quest y se agregaron las vistas del reviw

--- app/Http/Controllers/Store/ReviewController.php
+++ app/Http/Controllers/Store/ReviewController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Store;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ReviewRequest;
 use App\Models\Product;
+use App\Models\Review;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -24,16 +25,27 @@ class ReviewController extends Controller
             ]);
 
             DB::commit();
-            return response()->json(
-                [
-                    'message' => 'Review created successfully',
-                    'review' => $review
-                ],
-                201
-            );
+            return redirect()->route('products.details', $product->slug)->with('success', 'Review added successfully');
         } catch (\Exception $e) {
             DB::rollBack();
-            return response()->json(['message' => $e->getMessage()], 500);
+            return redirect()->route('products.details', $product->slug)->with('error', 'Error adding review');
+        }
+    }
+
+    public function update(ReviewRequest $request, string $id)
+    {
+        DB::beginTransaction();
+        $review = Review::findOrFail($id);
+        try {
+            $review->update([
+                'rating' => $request->rating,
+                'comment' => $request->comment,
+            ]);
+            DB::commit();
+            return redirect()->route('products.details', $review->product->slug)->with('success', 'Review updated successfully');
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return redirect()->route('products.details', $review->product->slug)->with('error', 'Error updating review');
         }
     }
 }
